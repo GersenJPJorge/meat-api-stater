38f16c052c6465c6bff5520a607b86f4e9d28bf8
Gersen James <gersen.jorge@gmail.com>
2019-03-01 18:26:06 -0300

    Criando componente usando controlValueAccessor(parte2)

--- src/app/shared/radio/radio.component.ts
+++ src/app/shared/radio/radio.component.ts
@@ -1,24 +1,52 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
 import { RadioOption } from './radio.model';
-import { setValueOnPath } from '@angular/platform-browser/src/browser/browser_adapter';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'mt-radio',
   templateUrl: './radio.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RadioComponent),
+      multi: true
+    }
+    ] 
+    // tudo isso foi feito para que o uso do controlvalueacessor fique acessivel as diretivas do ngmodel e 
+    // os modulos do reactiveform 
 })
-export class RadioComponent implements OnInit {
+export class RadioComponent implements OnInit, ControlValueAccessor {
 
   @Input() options: RadioOption []                     // como essas opções vêm de fora precisa do decorator @Input
 
   value: any
 
-  setValue(value: any){
-    this.value = value
-  }
+  onChange: any
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setValue(value: any){
+    this.value = value
+    this.onChange(this.value) // dessa forma eu aviso as diretivas que o valor do meu componente mudou
+  }
+
+  writeValue(obj: any): void {                   // método usado quando as diretivas querem passar um valor para o componente
+    this.value = obj;
+//    throw new Error("Method not implemented."); 
+  }
+  registerOnChange(fn: any): void {                   // necessário sempre que o valor do componente tiver uma mudança,
+    this.onChange = fn;                               // e essa mudança ocorre no nosso método "setvalue"  
+ //   throw new Error("Method not implemented.");  
+                                                  
+  }
+  registerOnTouched(fn: any): void {
+ //   throw new Error("Method not implemented.");
+  }
+  setDisabledState?(isDisabled: boolean): void {
+ //   throw new Error("Method not implemented.");
   }
 
 }
