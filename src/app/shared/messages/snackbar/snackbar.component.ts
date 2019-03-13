ff841ac5a1b7c395928ca5c0e8bfe6a27ce09d4f
Gersen James <gersen.jorge@gmail.com>
2019-03-13 13:31:08 -0300

    Usando os operadores Do e SwitchMap

--- src/app/shared/messages/snackbar/snackbar.component.ts
+++ src/app/shared/messages/snackbar/snackbar.component.ts
@@ -3,6 +3,8 @@ import { trigger, state, transition, style, animate } from '@angular/animations'
 import { NotificationService } from '../notitication.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/timer';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'mt-snackbar',
@@ -35,13 +37,14 @@ export class SnackbarComponent implements OnInit {
 
   ngOnInit() {                                                          // o ponto ideal para se inscreve é no ngOniti porque
                                                                         // todas as dependencias já foram carregadas
-      this.notificationService.notifier.subscribe                       // o event emmitter é um observable, por isso tem subscribe
-           (message=>{this.message = message
-                      this.snackVisibility = 'visible'
-                      Observable.timer(3000).subscribe(timer=> this.snackVisibility = 'hidden') // quando chegar o subscribe a gente vai
-                    })                                                                          // comecar a contar o time de 3 segundos
-     }                                                                                          // e quando atingir muda o status
-                                                                                                // todo observable precisa de um subscribe para funcionar 
+      this.notificationService.notifier
+            .do(message=>{
+                this.message = message
+                this.snackVisibility = 'visible'
+            }).switchMap(message => Observable.timer(3000))
+              .subscribe(timer => this.snackVisibility = 'hidden' )                                                                          
+     }                                                                                          
+                                                                                                 
 /* só para testar a animação
   toggleSnack(){
     this.snackVisibility = this.snackVisibility === 'hidden' ? 'visible' : 'hidden'
