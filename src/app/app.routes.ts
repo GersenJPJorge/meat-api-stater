7210b2acd9df38b55247346a26fffd723e52108b
Gersen James <gersen.jorge@gmail.com>
2019-03-13 18:30:34 -0300

    Rotas com Wildcard (Criando pagina não encontrada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { MenuComponent } from "./restaurant-detail/menu/menu.component";
 import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
 import { OrderComponent } from "./order/order.component";
 import { OrderSummaryComponent } from "./order-summary/order-summary.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 export const ROUTES: Routes = [
     {path: '', component: HomeComponent }, // componente padrão caso não se digite nada
@@ -18,7 +19,9 @@ export const ROUTES: Routes = [
             ] },
     {path: 'order', loadChildren: './order/order.module#OrderModule' },
     {path: 'order-summary', component: OrderSummaryComponent },
-    {path: 'about', loadChildren: './about/about.module#AboutModule'}
+    {path: 'about', loadChildren: './about/about.module#AboutModule'},
+    {path: '**', component: NotFoundComponent}                             // wildcard - quando não há nenhuma rota
+                                                                           // tem que ficar no final por causa o router do angular
 //    {path: 'about', loadChildren: '@Meat/about/about.module#AboutModule'}
     ]
 // com a alteração do children devemos ir no componente principal de restaurant-detail e colocar o router-outlet para
